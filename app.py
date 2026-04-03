bc733cad7e7b07f44d375125ececc5818ebd7cd9
om7621 <111795625+om7621@users.noreply.github.com>
2026-04-03 22:41:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -210,17 +210,18 @@ def export_cps_summary():
             "Cap Bank CF1", "Cap Bank CF2", "Cap Bank CF3", "Cap Bank CF4", "Cap Bank CF5", "Cap Bank CF6",
             # POWER SUPPLY
             "PS1 – 24V", "PS2 – 24V", "PS3 – 24V", "PS4 – 15V", "PS5 – 12V", "PS6 – 24V", "PS7 – 15V", "PS8 – +/-12V", "PS9 – 24V", "PS10 – 15V", "HMI",
-            
-
-            
-
-
-            "SKYPER1-U1", "SKYPER2-U1", "SKYPER3-U1", "SKYPER4-U1",
-            "SKYPER1-V1", "SKYPER2-V1", "SKYPER3-V1", "SKYPER4-V1",
-            "SKYPER1-W1", "SKYPER2-W1", "SKYPER3-W1", "SKYPER4-W1",
-            "SKYPER1-U2", "SKYPER2-U2", "SKYPER3-U2", "SKYPER4-U2",
-            "SKYPER1-V2", "SKYPER2-V2", "SKYPER3-V2", "SKYPER4-V2",
-            "SKYPER1-W2", "SKYPER2-W2", "SKYPER3-W2", "SKYPER4-W2"
+            # IGBT STACK ASSEMBLY U1
+            "A4-1", "A4-2", "IGBT1", "IGBT2", "IGBT3", "IGBT4", "TS1 – 120°C", "TS2 – 120°C", "NTC1 – 10K", "CD1-8", "NTC1 – 10K", "SKYPER1-U1", "SKYPER2-U1", "SKYPER3-U1", "SKYPER4-U1",
+            # IGBT STACK ASSEMBLY V1
+            "A4-3", "A4-4", "IGBT5", "IGBT6", "IGBT7", "IGBT8", "TS3 – 120°C", "TS4 – 120°C", "NTC2 – 10K", "CD9-16", "NTC2 – 10K", "SKYPER1-V1", "SKYPER2-V1", "SKYPER3-V1", "SKYPER4-V1",
+            # IGBT STACK ASSEMBLY W1
+            "A4-5", "A4-6", "IGBT9", "IGBT10", "IGBT11", "IGBT12", "TS5 – 120°C", "TS6 – 120°C", "NTC3 – 10K", "CD17-24", "NTC3 – 10K", "SKYPER1-W1", "SKYPER2-W1", "SKYPER3-W1", "SKYPER4-W1",
+            # IGBT STACK ASSEMBLY U2
+            "A4-7", "A4-8", "IGBT13", "IGBT14", "IGBT15", "IGBT16", "TS7 – 120°C", "TS8 – 120°C", "CD25-32", "NTC4 – 10K", "SKYPER1-U2", "SKYPER2-U2", "SKYPER3-U2", "SKYPER4-U2",
+            # IGBT STACK ASSEMBLY V2
+            "A4-9", "A4-10", "IGBT17", "IGBT18", "IGBT19", "IGBT20", "TS9 – 120°C", "TS10 – 120°C", "CD33-40", "NTC5 – 10K", "SKYPER1-V2", "SKYPER2-V2", "SKYPER3-V2", "SKYPER4-V2",
+            # IGBT STACK ASSEMBLY W2
+            "A4-11", "A4-12", "IGBT21", "IGBT22", "IGBT23", "IGBT24", "TS11 – 120°C", "TS12 – 120°C", "CD41-48", "NTC6 – 10K", "SKYPER1-W2", "SKYPER2-W2", "SKYPER3-W2", "SKYPER4-W2",
         ]
 
         final_df.fillna('', inplace=True)
